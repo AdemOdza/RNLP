bc9c58de3401d75dd3beffd508f17cbd66acbeb6
AdemOdza <Albania1veleshta!>
2022-05-04 15:46:53 -0500

    Added sentence tokenization
    Added sentence tokenization using regex

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,19 @@ mod tests {
     fn test_word_tokenize_fail() {
         use crate::tokenize::tokenize;
         let tokenized = tokenize::word_tokenize("a b c");
+
         assert_ne!(tokenized.len(), 4);
+    }
+
+    #[test]
+    fn test_sent_tokenize(){
+        use crate::tokenize::tokenize;
+        let tokenized = tokenize::sent_tokenize("Let's go to the mall. I want to buy a shirt. It has to be blue.");
+        let len = tokenized.len();
+
+        assert_eq!(len, 3);
+        
+        let last_item = tokenized[..].last().copied().expect("Unpacking vector error");
+        assert_ne!(last_item, "");
     }
 }
--- src/tokenize/tokenize.rs
+++ src/tokenize/tokenize.rs
@@ -1,7 +1,24 @@
+
+use regex::Regex;
+
+pub fn sent_tokenize(s: &str) -> Vec<&str> {
+	let p = Regex::new("[!.?]").expect("Invalid Regex");
+	let split_sent: Vec<&str> = p.split(s).into_iter().collect();
+	let leng = split_sent.len();
+
+	if split_sent[leng - 1] == ""{
+		return split_sent[0..leng-1].to_vec();
+	}
+	split_sent
+}
+
+
+
+//Simple Tokenizers
 pub fn word_tokenize(s: &str) -> Vec<&str> {
 	s.split(" ").collect()
 }
 
-pub fn sent_tokenize(s: &str) -> Vec<&str> {
-	s.split(".").collect()
+pub fn line_tokenize(s: &str) -> Vec<&str> {
+	s.split("\n").collect()
 }
