743f5e78a7e3c721fe1a90d1a075d7659cf016f1
AdemOdza <AdemOdza@gmail.com>
2022-05-26 16:37:41 -0500

    Changed return types of tokenization functions

--- src/tokenize/tokenize.rs
+++ src/tokenize/tokenize.rs
@@ -1,24 +1,28 @@
 
 use regex::Regex;
 
-pub fn sent_tokenize(s: &str) -> Vec<&str> {
+pub fn sent_tokenize(s: String) -> Vec<String> {
 	let p = Regex::new("[!.?]").expect("Invalid Regex");
-	let split_sent: Vec<&str> = p.split(s).into_iter().collect();
+	let split_sent: Vec<&str> = p.split(s.as_str()).collect();
 	let leng = split_sent.len();
 
 	if split_sent[leng - 1] == ""{
-		return split_sent[0..leng-1].to_vec();
+		return split_sent[0..leng-1].to_vec().into_iter().map(|s| s.to_string()).collect();
 	}
-	split_sent
+	split_sent.into_iter().map(|s| s.to_string()).collect()
 }
 
 
 
 //Simple Tokenizers
-pub fn word_tokenize(s: &str) -> Vec<&str> {
-	s.split(" ").collect()
+pub fn word_tokenize(s: String) -> Vec<String> {
+	s.replace(".", " PERIOD ")
+	.replace("!", " EXCLAMATION ")
+	.replace("?", " QUESTION ")
+	.replace("  ", " ").split(" ").map(|s| s.to_string()).collect::<Vec<String>>()
+
 }
 
-pub fn line_tokenize(s: &str) -> Vec<&str> {
-	s.split("\n").collect()
+pub fn line_tokenize(s: String) -> Vec<String> {
+	s.split("\n").into_iter().map(|s| s.to_string()).collect()
 }
